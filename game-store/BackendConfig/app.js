3143fff89cd5e4c7cea51bedea47bd58f5a56cff
carlcallorina0929 <callorinajustin@gmail.com>
2026-04-19 22:21:52 +0800

    Added images folder in backend

--- game-store/BackendConfig/app.js
+++ game-store/BackendConfig/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use(cors());
 app.use(morgan("dev"));
